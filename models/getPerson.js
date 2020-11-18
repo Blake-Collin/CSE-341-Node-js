1efe72697b2e3b9b4a3b4e49c6857fb54eb9964b
Collin Blake <collinblake1@gmail.com>
2020-11-17 20:14:19 -0700

    test

--- models/getPerson.js
+++ models/getPerson.js
@@ -2,12 +2,14 @@
 const { Pool } = require('pg');
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connectionString});
+//process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
 
 module.exports = { request: function (req, res) {
         if(req.query.id)
         {
             const id = req.query.id;            
-            pool.query('SELECT * FROM person WHERE id = $1', id, (err, res) => {
+            pool.query('SELECT * FROM person WHERE id = $1', [id], (err, res) => {
                 if (err) {
                     throw err
                   }
